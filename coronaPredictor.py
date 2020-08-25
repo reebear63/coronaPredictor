cd03cca00cf43401c2cdd3482fd99f3a4c18bdd1
reebear63 <66818460+reebear63@users.noreply.github.com>
2020-08-25 17:09:49 +0200

    working version

--- coronaPredictor.py
+++ coronaPredictor.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 from sklearn.preprocessing import PolynomialFeatures
-#from sklearn import
+from sklearn import linear_model
 
 #### LOAD DATA ####
 data = pd.read_csv('c:\\Users\\andreasg\\Documents\\Golgath\\Python\\corona\\coronaCases.csv', sep=',')
@@ -21,6 +21,31 @@ y = np.array(data['cases']).reshape(-1,1)
 plt.plot(y, '-m')
 plt.show()
 
-polyFeat = PolynomialFeatures(degree=2)
+polyFeat = PolynomialFeatures(degree=3)
 x = polyFeat.fit_transform(x)
-print(x.head)
+#print(x.head)
+
+#### TRAINING DATA ####
+print('-'*30)
+print('TRAINING DATA')
+print('-'*30)
+model = linear_model.LinearRegression()
+model.fit(x,y)
+accuracy = model.score(x,y)
+print(f'Accuracy: {round(accuracy*100,3)} %')
+y0 = model.predict(x)
+
+#### PREDICTION ####
+days = 15
+print('-'*30)
+print('PREDICTION')
+print('-'*30)
+print (f'Prediction - cases after (days) days:  ', end='')
+print(round(int(model.predict(polyFeat.fit_transform([[234+days]])))/1000000,2), 'Million')
+
+x1 = np.array(list(range(1,234+days))).reshape(-1,1)
+y1 = model.predict(polyFeat.fit_transform(x1))
+plt.plot(y1, '--r')
+plt.plot(y0, '--b')
+plt.show()
+
--- readme.md
+++ readme.md
@@ -1,2 +1,11 @@
 coronaPredictor project
 (by Murtaza Hassan)
+
+https://www.murtazahassan.com/courses/machine-learning-with-python/lesson/polynomial-regression-covid-19-predictor/
+
+echo "# coronaPredictor" >> README.md
+git init
+git add README.md
+git commit -m "first commit"
+git remote add origin https://github.com/reebear63/coronaPredictor.git
+git push -u origin master
